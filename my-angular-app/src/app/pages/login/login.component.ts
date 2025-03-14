defb941ff20bb260e45dc66688e8de85176b0bde
GiuseppeSindoni <g.sindoni@studenti.unina.it>
2025-03-14 17:56:32 +0100

    Modifica responsive

--- my-angular-app/src/app/pages/login/login.component.ts
+++ my-angular-app/src/app/pages/login/login.component.ts
@@ -5,12 +5,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, RouterModule, FooterComponent],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
