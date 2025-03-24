bd938b4b7517bd9654ea70f5a6448887e6fb1cff
GiuseppeSindoni03 <giuseppenapolii55@gmail.com>
2025-03-24 11:17:43 +0100

    Delete hive-mind-server directory

--- hive-mind-server/dist/routes/vote.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const vote_controller_1 = require("../controllers/vote.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/:id/vote", auth_middleware_1.authenticateToken, vote_controller_1.setVote);
-router.get("/:id/vote", auth_middleware_1.authenticateToken, vote_controller_1.getUserVote);
-exports.default = router;
